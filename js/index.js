9c5cad17db935766abc9b1571d73a4f2cd01f64a
Carter Capocaccia <Carter.Capocaccia@Gmail.com>
2017-11-17 16:30:21 -0600

    adds some funky properties making the colors change, line width change, and learning about the globalCompositeOperation property

--- js/index.js
+++ js/index.js
@@ -7,20 +7,26 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 50;
+ctx.globalCompositeOperation = 'multiply';
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
 
 function draw(e){
 	if(!isDrawing) return;
+	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 	ctx.beginPath();
 	ctx.moveTo(lastX, lastY);
 	ctx.lineTo(e.offsetX, e.offsetY);
 	[lastX, lastY] = [e.offsetX, e.offsetY];
 	ctx.stroke();
+	hue >= 360 ? hue = 0 : hue++;
+	ctx.lineWidth = hue;
 }
+
+
 
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mousedown', (e) => {
