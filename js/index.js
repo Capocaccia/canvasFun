2c4893a1a4b103aa4ae02d41a97f1955f3065ee1
Carter Capocaccia <carter.capocaccia@gmail.com>
2017-11-23 19:16:19 -0600

    adds js to clear canvas on click, color pallette, and size select

--- js/index.js
+++ js/index.js
@@ -1,5 +1,28 @@
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
+const clearButton = document.querySelector('.clear');
+const colors = document.querySelectorAll('.color');
+const widthSelect = document.querySelector('.width-select');
+
+widthSelect.addEventListener('click', function(){
+	ctx.lineWidth = widthSelect.value * 5;
+});
+
+
+colors.forEach((color) => {
+	var currentColor = color.dataset.color;
+	color.style.width = '25px';
+	color.style.height = '25px';
+	color.style.backgroundColor = currentColor;
+
+	color.addEventListener('click', function(){
+		ctx.strokeStyle = currentColor;
+	});
+});
+
+clearButton.addEventListener('click', function(){
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -7,7 +30,6 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.globalCompositeOperation = 'multiply';
 
 let isDrawing = false;
 let lastX = 0;
@@ -16,14 +38,11 @@ let hue = 0;
 
 function draw(e){
 	if(!isDrawing) return;
-	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 	ctx.beginPath();
 	ctx.moveTo(lastX, lastY);
 	ctx.lineTo(e.offsetX, e.offsetY);
 	[lastX, lastY] = [e.offsetX, e.offsetY];
 	ctx.stroke();
-	hue >= 360 ? hue = 0 : hue++;
-	ctx.lineWidth = hue;
 }
 
 
@@ -32,7 +51,7 @@ canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mousedown', (e) => {
 	isDrawing = true;
 	[lastX, lastY] = [e.offsetX, e.offsetY];
-
 });
+
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
